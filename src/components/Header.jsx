50ea80bde092105da804a57550f8a2cdacab293b
Deepak <sinhadeepak619@gmail.com>
2025-03-06 09:29:13 +0530

    Styled Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { Navbar } from "./Navbar";
+import styled from "styled-components";
+
+export const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/" className="logo">
+        Deepak
+      </NavLink>
+
+      <Navbar></Navbar>
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: auto;
+    max-width: 30%;
+    font-size: 30px;
+    text-decoration: none;
+    color: Blue;
+  }
+`;
